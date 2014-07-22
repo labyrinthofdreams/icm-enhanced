371cdfff73719773ac61695d8c08b9958cb6a70c
labyrinthofdreams <noemail@localhost.localhost>
2014-07-22 14:59:46 +0300

    Update version number to 1.7

--- icm-enhanced.user.js
+++ icm-enhanced.user.js
@@ -2,7 +2,7 @@
 // @name           iCheckMovies Enhanced
 // @namespace      iCheckMovies
 // @description    Adds new features to enhance the iCheckMovies user experience
-// @version        1.6.1.1
+// @version        1.7
 // @include        http://icheckmovies.com*
 // @include        http://www.icheckmovies.com*
 // @include        https://icheckmovies.com*
@@ -126,8 +126,8 @@ ICM_BaseFeature.prototype.updateConfig = function(config) {
 function ICM_Config() {
     this.cfg = {
         script_config: { // script config
-            version: "1.6.1.1",
-            revision: 1611 // numerical representation of version number
+            version: "1.7",
+            revision: 1700 // numerical representation of version number
         }
     };
 
